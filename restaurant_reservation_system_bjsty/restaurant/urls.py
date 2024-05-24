34f953bb005274a00437e19056b20f98206513d9
Benedikt-Mueller <benedikt.mueller2904@gmail.com>
2024-05-24 12:58:26 +0200

    Added ER-Model, Added createUser View, Added URL create_user

--- restaurant_reservation_system_bjsty/restaurant/urls.py
+++ restaurant_reservation_system_bjsty/restaurant/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [ 
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("create_user", views.createUser, name="create_user")
 ]
--- restaurant_reservation_system_bjsty/restaurant/views.py
+++ restaurant_reservation_system_bjsty/restaurant/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Guten Tag. Sie befinden sich auf der Hauptseite dieses Restaurant-Reservierungssystems!")
+def createUser(request):
+    return HttpResponse("Erstellen sie einen Benutzer!")
